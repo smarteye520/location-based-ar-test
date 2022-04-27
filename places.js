a4e30a81d16bef95d97e163ce76f19ecb0219196
Smart Eye <smarteye0701@gmail.com>
2022-04-27 05:26:54 -0400

    Rendered icon.

--- places.js
+++ places.js
@@ -81,52 +81,51 @@ function renderPlaces(places) {
         const latitude = place.location.lat;
         const longitude = place.location.lng;
 
-        // // add place icon
-        // const icon = document.createElement('a-image');
-        // icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
-        // icon.setAttribute('name', place.name);
-        // icon.setAttribute('src', './assets/map-marker.png');
-        // // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
-        // icon.setAttribute('scale', '0.1 0.1 0.1');
-        // icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
+        // add place icon
+        const icon = document.createElement('a-image');
+        icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
+        icon.setAttribute('name', place.name);
+        icon.setAttribute('src', './assets/map-marker.png');
+        // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
+        icon.setAttribute('scale', '1 1 1');
+        icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
 
-        const placeText = document.createElement('a-link');
-        placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        placeText.setAttribute('title', place.name);
-        placeText.setAttribute('scale', '15 15 15');
+        const clickListener = function (ev) {
+            ev.stopPropagation();
+            ev.preventDefault();
+
+            const name = ev.target.getAttribute('name');
+
+            const el = ev.detail.intersection && ev.detail.intersection.object.el;
+
+            if (el && el === ev.target) {
+                const label = document.createElement('span');
+                const container = document.createElement('div');
+                container.setAttribute('id', 'place-label');
+                label.innerText = name;
+                container.appendChild(label);
+                document.body.appendChild(container);
+
+                setTimeout(() => {
+                    container.parentElement.removeChild(container);
+                }, 5500);
+            }
+        };
+
+        icon.addEventListener('click', clickListener);
+
+        scene.appendChild(icon);
+
+        // const placeText = document.createElement('a-link');
+        // placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+        // placeText.setAttribute('title', place.name);
+        // placeText.setAttribute('scale', '15 15 15');
         
-        placeText.addEventListener('loaded', () => {
-            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
-        });
+        // placeText.addEventListener('loaded', () => {
+        //     window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+        // });
 
-        scene.appendChild(placeText);
+        // scene.appendChild(placeText);      
 
-        
-
-        // const clickListener = function (ev) {
-        //     ev.stopPropagation();
-        //     ev.preventDefault();
-
-        //     const name = ev.target.getAttribute('name');
-
-        //     const el = ev.detail.intersection && ev.detail.intersection.object.el;
-
-        //     if (el && el === ev.target) {
-        //         const label = document.createElement('span');
-        //         const container = document.createElement('div');
-        //         container.setAttribute('id', 'place-label');
-        //         label.innerText = name;
-        //         container.appendChild(label);
-        //         document.body.appendChild(container);
-
-        //         setTimeout(() => {
-        //             container.parentElement.removeChild(container);
-        //         }, 1500);
-        //     }
-        // };
-
-        // icon.addEventListener('click', clickListener);
-
-        // scene.appendChild(icon);
     });
 }
