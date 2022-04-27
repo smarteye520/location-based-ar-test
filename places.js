8ede27b0911050e449b79f98d471d195cee3e620
Smart Eye <smarteye0701@gmail.com>
2022-04-27 05:02:22 -0400

    Updated icons.

--- places.js
+++ places.js
@@ -88,7 +88,7 @@ function renderPlaces(places) {
         icon.setAttribute('src', './assets/map-marker.png');
 
         // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
-        icon.setAttribute('scale', '20, 20');
+        icon.setAttribute('scale', '1, 1');
 
         icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
 
