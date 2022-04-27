566fe8297da5c958df426ba542a7e64c51b62da8
Smart Eye <smarteye0701@gmail.com>
2022-04-27 05:21:48 -0400

    Updated Scale.

--- places.js
+++ places.js
@@ -93,7 +93,7 @@ function renderPlaces(places) {
         const placeText = document.createElement('a-link');
         placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
         placeText.setAttribute('title', place.name);
-        placeText.setAttribute('scale', '5 5 5');
+        placeText.setAttribute('scale', '15 15 15');
         
         placeText.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
