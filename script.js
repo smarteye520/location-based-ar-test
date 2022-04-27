3b35ac03dcbfcf010971ebf9df59049fdb65f1de
Smart Eye <smarteye0701@gmail.com>
2022-04-27 05:58:42 -0400

    Removed moving animation.

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ function renderPlaces(places) {
 
         setModel(models[0], model);
 
-        model.setAttribute('animation-mixer', '');
+        // model.setAttribute('animation-mixer', '');
 
         // document.querySelector('button[data-action="change"]').addEventListener('click', function () {
         //     var entity = document.querySelector('[gps-entity-place]');
